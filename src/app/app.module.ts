25f6d73893cc60f882787a8da0467657a5c5bcdc
wandile <wandile.chamane@gmail.com>
2018-08-16 17:08:01 +0200

    init commit to test keycloak-angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,125 @@
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { AppComponent } from './app.component';
+
+import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
+import { initializer } from './keycloak/keycloak-init';
+
+import { SidebarModule } from './sidebar/sidebar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule} from './shared/navbar/navbar.module';
+import { FixedpluginModule} from './shared/fixedplugin/fixedplugin.module';
+import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
+
+import { AppRoutes } from './app.routing';
+
+@NgModule({
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule
+  ]
+})
+export class MaterialModule {}
+
+@NgModule({
+    imports:      [
+        CommonModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        RouterModule.forRoot(AppRoutes),
+        HttpModule,
+        MaterialModule,
+        MatNativeDateModule,
+        SidebarModule,
+        NavbarModule,
+        FooterModule,
+        FixedpluginModule,
+        KeycloakAngularModule
+    ],
+    declarations: [
+        AppComponent,
+        AdminLayoutComponent,
+        AuthLayoutComponent
+    ],
+    bootstrap:    [ AppComponent ],
+    providers: [
+      {
+        provide: APP_INITIALIZER,
+        useFactory: initializer,
+        multi: true,
+        deps: [KeycloakService]
+      }
+    ]
+})
+export class AppModule { }
+
+
